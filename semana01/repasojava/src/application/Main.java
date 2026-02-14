5f731f70368f30c1154ecdb05e3e5b66fbd60767
Santiago Salazar <santiago.sk08@gmail.com>
2026-02-14 12:04:23 -0500

    definición de variables y metodos

--- semana01/repasojava/src/application/Main.java
+++ semana01/repasojava/src/application/Main.java
@@ -1,33 +1,24 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class    Main {
-
+    final static int CURRENT_YEAR = 2026;
     public static void main(String[] args) {
-        System.out.println("Hello, ");
-        String name;
-        int age;
-        boolean male;
-        int BIRTH_YEAR = 1996;
 
-        name = "Santiago";
-        age = 30;
-        male = true;
-        int birth_year = 1995;
-
-        System.out.println("My name is " + name);
-        System.out.println("I am " + age + " years old");
-        System.out.println("Am I male?" + male );
-
-        final int CURRENTYEAR = 2026;
-        int birthYear = 1995;
-        int calculateAge = CURRENTYEAR - BIRTH_YEAR;
-        System.out.println( "Calculated age: " + calculateAge);
-
-        for (int i = 1; i <= 10;i++) {
-            System.out.println("Itineration: " + 1) ;
-        }
-
+        final String GREETING_MESSAGE = "Saludo Estudiantes de Java"
+                String [] namesStudents = {"Juan", "Maria", "Pedro", "Ana", "Luis"};
+        int [] birthYears = {2000, 1998, 2002, 1995, 2001};
+        List<int[]> agesStudentList = new ArrayList<>();
     }
 
+    void showMessage(String message){
+        System.out.println(message);
+    }
+
+    static int calculatedAge   (int birthYear) {
+        return CURRENT_YEAR - birthYear;
+    }
 
 }

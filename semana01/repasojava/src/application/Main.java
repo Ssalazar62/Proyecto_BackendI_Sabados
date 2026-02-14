ea8b4ccd582075561e45b66465d52d21785106d0
Santiago Salazar <santiago.sk08@gmail.com>
2026-02-14 13:57:03 -0500

    Ejercicio finalizado semana 03

--- semana01/repasojava/src/application/Main.java
+++ semana01/repasojava/src/application/Main.java
@@ -5,20 +5,41 @@ import java.util.List;
 
 public class    Main {
     final static int CURRENT_YEAR = 2026;
+    static List<Integer> agesStudentList = new ArrayList<>();
     public static void main(String[] args) {
 
-        final String GREETING_MESSAGE = "Saludo Estudiantes de Java"
+        final String GREETING_MESSAGE = "Saludo Estudiantes de Java";
                 String [] namesStudents = {"Juan", "Maria", "Pedro", "Ana", "Luis"};
         int [] birthYears = {2000, 1998, 2002, 1995, 2001};
-        List<int[]> agesStudentList = new ArrayList<>();
-    }
 
-    void showMessage(String message){
+        // 1. Mostramos el saludo
+        showMessage(GREETING_MESSAGE);
+
+        // 2. Llenamos la lista de edades usando el arreglo de años
+        fillAgesList(birthYears);
+
+        // 3. Mostramos la lista final combinando nombres y edades
+        listStudents(namesStudents, agesStudentList);
+    }
+    static void showMessage(String message){
         System.out.println(message);
     }
 
     static int calculatedAge   (int birthYear) {
         return CURRENT_YEAR - birthYear;
+    }
+    static List<Integer> fillAgesList( int[] birthYears) {
+        for (int birthYear : birthYears) {
+            int age = calculatedAge(birthYear);
+            agesStudentList.add(age);
+        }
+        return agesStudentList;
+    }
+    static void listStudents(String [] namesStudents, List <Integer> agesStudents) {
+        showMessage("Lista de estudiantes");
+        for (int i = 0; i < namesStudents.length; i++){
+            showMessage(namesStudents[i] + " - Edad: " + agesStudents.get(i));
+        }
     }
 
 }
